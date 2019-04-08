7c174fc35f74c151633d4cbab4051822ff41687f
YoshikazuSAKANA <koushi1105@gmail.com>
2019-04-08 20:39:40 +0900

    JS追加(期限日表示)

--- src/htdocs/index.php
+++ src/htdocs/index.php
@@ -8,6 +8,11 @@
 require_once('/home/y/share/pear/blueplum/src/config/Config.php');
 require_once(_ROOT_DIR . '/app/Dispatcher.php');
 
+if (isset($_COOKIE[_MEMBER_SESSNAME])) {
+    session_name(_MEMBER_SESSNAME);
+    session_start();
+}
+
 $dispatcher = new Dispatcher();
 $dispatcher->dispatch($conf);
 
---
+++ src/htdocs/js/top.js
@@ -0,0 +1,50 @@
+setInterval('showTimeLimit(2019,4,21)',1000);
+
+function showClock() {
+    // 現在時刻を取得
+    let nowTime = new Date();
+
+    // 時を抜き出す
+    let nowHour = nowTime.getHours();
+
+    // 分を抜き出す
+    let nowMin = nowTime.getMinutes();
+
+    // 秒を抜き出す
+    let nowSec = nowTime.getSeconds();
+
+    let msg = "現在時刻は、" + nowHour + ":" + nowMin + ":" + nowSec;
+
+    document.getElementById("RealTimeClockArea") . innerHTML = msg;
+}
+
+setInterval('showClock()',1000);
+
+function showTimeLimit(tYear, tMonth, tDay) {
+    // 現在時刻を数値に変換
+    let nowTime = new Date();
+    dnumNow = nowTime.getTime();
+
+    // 納期
+    let targetDay = tYear + "/" + tMonth + "/" + tDay
+
+    // 指定日時を数値に変換
+    let targetTime = new Date(targetDay);
+    dnumTarget = targetTime.getTime();
+
+    let diffMSec = dnumTarget - dnumNow;
+    let diffHour = diffMSec / (1000 * 60 * 60);
+    let diffDay = diffMSec / (1000 * 60 * 60 * 24);
+
+    let limitDay = Math.floor(diffDay);
+    let limitHour = diffHour - (limitDay * 24);
+    let limitMin = (limitHour - Math.floor(limitHour)) * 60;
+    let limitSec = (limitMin - Math.floor(limitMin)) * 60;
+
+    limitHour = Math.floor(limitHour);
+    limitMin = Math.floor(limitMin);
+    limitSec = Math.floor(limitSec);
+
+    let msg = targetDay + " まであと" + limitDay + "日" + limitHour + "時間" + limitMin + "分" + limitSec + "秒";
+    document.getElementById("LimitTimeArea") . innerHTML = msg;
+}
