03a902d49ab7645b985e77be4e44c0560219647c
YoshikazuSAKANA <koushi1105@gmail.com>
2019-09-04 18:20:57 +0900

    アクセスログをメールで取得（バッチファイル）

--- batch/add_time.php
+++ batch/add_time.php
@@ -2,14 +2,33 @@
 /*
  * クローン実行ファイル.
  * 1時間毎に指定ファイルに時間を書き出すだけの不毛ファイル
- *
+ * X時59分に実行
  */
 
 date_default_timezone_set('Asia/Tokyo');
 
-$file = '/home/y/share/pear/blueplum/src/app/View/admin_test_cron.html';
+$week = [
+  '日', //0
+  '月', //1
+  '火', //2
+  '水', //3
+  '木', //4
+  '金', //5
+  '土', //6
+];
+$weekNum = date('w');
+$nowDate = date("Y/m/d H:i:s");
 
-$current = file_get_contents($file);
-$current .= date("Y-m-d H:i:s") . "<BR>";
+// 取得するアクセスログの時間
+$getAccessErrorLog = date('Y/m/d H', strtotime("-1 hour"));
+$file = '/home/y/share/pear/blueplum/log/access.log';
 
-//file_put_contents($file, $current);
+$fp = fopen($file, 'r')
+while($line = fgets($fp)) {
+    $accessErrorDateUntilHour = strstr($line, ':', true);
+    if ($accessErrorDateUntilHour == $getAccessErrorLog) {
+      $message .= "{$line}\n";
+    }
+}
+
+mb_send_mail('koushi1105@gmail.com', "[{$nowDate}({$week[$weekNum]})]アクセスログ", $message, 'From: huitawarosu@yahoo.co.jp');
