47594c4924a2cf67cecbc54103bd94305a115ea2
YoshikazuSAKANA <koushi1105@gmail.com>
2019-09-05 10:50:25 +0900

    閉じ忘れ修正

--- src/app/Model/WebAPI.php
+++ src/app/Model/WebAPI.php
@@ -61,4 +61,3 @@ class Yahoo implements WebAPI {
 
 }
 
-}
--- src/htdocs/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-$tmp = 'tmp/kyousipanda.jpg';
-if (rename($tmp, 'image/kyousipanda.jpg')) {
-    echo "OK";
-} else {
-    echo "NO";
-}
