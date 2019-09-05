e4dea7330369954c9b3f64c20d6e2f70030098e8
YoshikazuSAKANA <koushi1105@gmail.com>
2019-09-05 10:58:54 +0900

    ファイル名変更（商品API検索結果）、YahooAPI実行アクション追加

--- src/app/Controller/ProductController.php
+++ src/app/Controller/ProductController.php
@@ -8,25 +8,19 @@
  */
 class ProductController {
 
-    /**
-     * 商品詳細を抽出＆表示.
-     *
-     * @access public
-     * @param int $carId
-     */
-    public function dispCarDetailAction($carId) {
-
-        $DBModel = new DBModel();
-        $product = $DBModel->searchProductDetail($carId);
-
-        require_once(_VIEW_DIR . '/detail.html');
-    }
-
     public function searchBooksAction() {
 
         $Rakuten = new Rakuten();
         $author = mb_convert_kana(htmlspecialchars($_POST['author'], ENT_QUOTES, 'UTF-8'), 's');
         $rakutenItem = $Rakuten->searchItem($author);
         require_once(_VIEW_DIR . '/search_book.php');
+    }
+
+    public function searchItemAction() {
+
+      $item = htmlspecialchars($_POST['item'], ENT_QUOTES, 'UTF-8');
+      $Yahoo = new Yahoo();
+      $Yahoo->searchItem($item);
+      require_once(_VIEW_DIR . '/search_book.php');
     }
 }
